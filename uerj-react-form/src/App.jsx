c95369a4350153349e0f78249d479012e1deed5b
carlosalberto_filho <carlosalberto_filho@outlook.com>
2023-05-30 22:08:19 +0000

    feat: App using Column and Sections

--- uerj-react-form/src/App.jsx
+++ uerj-react-form/src/App.jsx
@@ -1,9 +1,31 @@
 import "bulma/css/bulma.min.css";
 import "./App.css";
-
+import { Section } from "./components/layout/Section";
+import { Columns, Column } from "./components/layout/Columns";
 
 export const App = () => {
-
-  return(<>
-  </>)
+  return (
+    <>
+      <Section>
+        <h1 className="title">Formulário de Incrição</h1>
+        <p className="subtitle">Treinamento de React</p>
+      </Section>
+      <Section>
+        <form>
+          <Columns>
+            <Column> nome</Column>
+            <Column> email</Column>
+          </Columns>
+          <Columns>
+            <Column> região</Column>
+            <Column> estados</Column>
+            <Column> município</Column>
+          </Columns>
+          <Columns>
+            <Column> botões</Column>
+          </Columns>
+        </form>
+      </Section>
+    </>
+  );
 };
